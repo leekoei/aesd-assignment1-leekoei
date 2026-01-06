f4cb4bb28f2f8de2e3d91f5b175e43392b16b4a5
ronli <ronli@Ubuntu.myguest.virtualbox.org>
2026-01-06 11:07:25 -0500

    - Fix no_llseek not available in kernel 6.12 issue.

--- scull/pipe.c
+++ scull/pipe.c
@@ -311,7 +311,7 @@ static struct file_operations scullpipe_proc_ops = {
  */
 struct file_operations scull_pipe_fops = {
 	.owner =	THIS_MODULE,
-	.llseek =	no_llseek,
+	.llseek =	noop_llseek,
 	.read =		scull_p_read,
 	.write =	scull_p_write,
 	.poll =		scull_p_poll,
